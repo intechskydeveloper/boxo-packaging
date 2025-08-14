95bc28301eccdcaa107978114ff7cc653d906647
developer intechsky <intechskydeveloper@gmail.com>
2025-08-15 01:32:52 +0500

    Minor chagnes

--- app/components/sections/Home/FeaturedSection.tsx
+++ app/components/sections/Home/FeaturedSection.tsx
@@ -1,7 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { ArrowRight } from "lucide-react";
 import Image from "next/image";
-import Link from "next/link";
 import * as React from "react";
 import {
   Carousel,
